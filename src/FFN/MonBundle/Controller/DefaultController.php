9565ee0419a07d361589c1a82cb6aa4ab387948d
Nicolas Sias <nicolas.sias@gmail.com>
2012-12-26 16:40:37 +0100

    Feature: Premier commit relatif à l'interface

--- src/FFN/MonBundle/Controller/DefaultController.php
+++ src/FFN/MonBundle/Controller/DefaultController.php
@@ -3,17 +3,12 @@
 namespace FFN\MonBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/hello/{name}")
-     * @Template()
-     */
-    public function indexAction($name)
-    {
-        return array('name' => $name);
-    }
+
+  public function homeAction(){
+    return $this->render('FFNMonBundle:Page:home.html.twig', array(
+    ));
+  }
 }
