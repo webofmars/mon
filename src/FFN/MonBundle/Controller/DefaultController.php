1d0527db908a283a6e2aec498d3316a9b57d8861
Nicolas Sias <nicolas.sias@gmail.com>
2012-12-26 17:52:25 +0100

    Feature: Ajout de la gestion des utilisateurs - a compléter

--- src/FFN/MonBundle/Controller/DefaultController.php
+++ src/FFN/MonBundle/Controller/DefaultController.php
@@ -4,21 +4,37 @@ namespace FFN\MonBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use FFN\MonBundle\Entity\User;
+
 class DefaultController extends Controller
 {
-
-  public function homeAction(){
+  public function adminUserAction(){
+    $em = $this->get('doctrine')->getEntityManager();
+    $users = $em->getRepository('FFN\MonBundle\Entity\User')->findAll();
+    
+    return $this->render('FFNMonBundle:Page:admin_user.html.twig', array(
+      'users' => $users,
+    ));
+  }
+  
+  public function adminUserAddAction(){
     return $this->render('FFNMonBundle:Page:home.html.twig', array(
     ));
   }
   
-  public function userAddAction(){
+  public function adminUserDelete($id){
+    echo 'TODO';
+    return $this->render('FFNMonBundle:Page:home.html.twig', array(
+    ));
+  }
+    
+  public function adminUserEditAction($id){
     echo 'TODO';
     return $this->render('FFNMonBundle:Page:home.html.twig', array(
     ));
   }
   
-  public function userManageAction(){
+  public function homeAction(){
     echo 'TODO';
     return $this->render('FFNMonBundle:Page:home.html.twig', array(
     ));
