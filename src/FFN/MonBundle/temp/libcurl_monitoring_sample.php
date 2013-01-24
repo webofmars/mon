44b8a7e1389941133d699042f2beedd2c8578fdf
FredLeger <leger.frederic@gmail.com>
2013-01-25 00:44:20 +0100

    - fixed naming conventions typos - added dynamic graphs - changes in scheduler update

---
+++ src/FFN/MonBundle/temp/libcurl_monitoring_sample.php
@@ -0,0 +1,35 @@
+﻿<?php
+
+  // Création d'un gestionnaire curl
+  $ch = curl_init('http://test-fleger.prod.mobivillage.com/');
+  
+  // pas de sortie
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  
+  // Pour la securité
+  curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, 5 );
+  curl_setopt( $ch, CURLOPT_TIMEOUT, 20 );
+  curl_setopt( $ch, CURLOPT_MAXREDIRS, 5 );
+
+  // Exécution
+  $reponse = curl_exec($ch);
+  
+  // Vérification si une erreur est survenue
+  if(!curl_errno($ch))
+  {
+    $info = curl_getinfo($ch);
+    echo '[';
+    echo chr(39).date('H:i:s').chr(39);
+    echo chr(44);
+    echo $info['connect_time'];
+    echo chr(44);
+    echo $info['namelookup_time'];
+    echo chr(44);
+    echo $info['starttransfer_time'];
+    echo chr(44);
+    echo $info['total_time'];
+    echo ']';
+  }
+
+  // Fermeture du gestionnaire
+  curl_close($ch);
