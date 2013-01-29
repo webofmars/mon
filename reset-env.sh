907cbfef3dd3ec6d3d3737304000934d9fa6baf9
FredLeger <leger.frederic@gmail.com>
2013-01-29 23:08:19 +0100

    - added a small script to clean the environement

---
+++ reset-env.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+php app/console cache:clear --no-warmup --env=dev
+php app/console cache:clear --no-warmup --env=prod
+php app/console doctrine:database:drop --force
+php app/console doctrine:database:create
+php app/console doctrine:schema:update --force
+php app/console doctrine:fixtures:load
+php app/console assets:install
+php app/console assetic:dump web
+php app/console mon:scheduler:update
