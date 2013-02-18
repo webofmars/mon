b140da1838bea3f2df89174271bbe6caf075ab55
Frederic Leger <leger.frederic@gmail.com>
2013-02-18 12:32:57 +0100

    - ajout de la table subscription

---
+++ src/FFN/MonBundle/DataFixtures/ORM/LoadSubscriptionData.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace FFN\MonBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use DateTime;
+use FFN\MonBundle\Entity\User;
+
+/**
+ * Description of LoadScenarioData
+ *
+ * @author frederic
+ */
+class LoadUserData  extends AbstractFixture implements OrderedFixtureInterface {
+    
+    public function load(ObjectManager $om) {
+        
+        $user = new User();
+        
+        $user->setUsername('admin');
+        $user->setEmail('dev@null.cz');
+        $user->setEnabled(true);
+        $user->setPlainPassword('JABE6mA3JUw7BSvQPCfG');
+        $user->setLocked(false);
+        $user->setExpired(false);
+        
+        $om->persist($user);
+        $om->flush();
+        
+        $this->setReference('user', $user);
+        
+    }
+    
+    public function getOrder() {
+        return(1);
+    }
+}
