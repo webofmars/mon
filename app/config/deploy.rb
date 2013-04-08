2b9c2d77126e71c1fdc6219a841c04573d108712
FredLeger <leger.frederic@gmail.com>
2013-04-08 20:49:00 +0200

    deploy.rb

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -3,22 +3,21 @@ set :domain,      "#{application}.jrns.fr"
 set :deploy_to,   "/var/www/#{application}"
 set :app_path,    "app"
 
-set :repository,  "file:///home/frederic/Dropbox/web of mars/projets/mon"
+set :repository,  "ssh://frederic.leger@mon.jrns.fr/opt/git/mon.git"
 set :scm,         :git
-set :deploy_via,  :copy
-
-set :model_manager, "doctrine"
 
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain                         # This may be the same as your `Web` server
 role :db,         domain, :primary => true       # This is where Symfony2 migrations will run
 
-set  :keep_releases,  3
+set :keep_releases,  3
 
-set  :user, "frederic.leger"
-set  :use_sudo,      true
-default_run_options[:pty] = true
-set  :git_enable_submodules, 0
+set :user, "frederic.leger"
+set :use_sudo, true
+set :use_composer, true
+
+#default_run_options[:pty] = true
+#set  :git_enable_submodules, 0
 
 set :shared_files,      ["app/config/parameters.ini"]
 set :shared_children,   [app_path + "/logs", web_path + "/uploads", "vendor"]
