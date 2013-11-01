eae52680bcad8b6a318359d2ba0d8835c76cc610
U-louravi\frederic <frederic@louravi.(none)>
2013-11-01 19:23:12 +0100

    - fixed issue #21 : graph_simple is showing nothing - all dates are stored in UTC and displayed in the user prefered TZ (added in the user settings, still need to add it in the user form)

--- src/FFN/MonBundle/Controller/GraphController.php
+++ src/FFN/MonBundle/Controller/GraphController.php
@@ -7,6 +7,7 @@ use FFN\MonBundle\Entity\Project as ProjectEntity;
 use FFN\MonBundle\Entity\Scenario as ScenarioEntity;
 use FFN\MonBundle\Entity\Control as ControlEntity;
 use \DateTime;
+use \DateTimeZone;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 /**
@@ -16,17 +17,22 @@ use Symfony\Component\Security\Core\Exception\AccessDeniedException;
  */
 class GraphController extends Controller {
 
-  public function showAction($control_id, $startTs = null, $stopTs = null) {
-
+  public function showAction($control_id, \DateTime $startTs = null, \Datetime $stopTs = null) {
+    
+    $startTs->setTimezone(new DateTimeZone('UTC'));
+    $stopTs->setTimezone(new DateTimeZone('UTC'));
+    
+    $user = $this->get('security.context')->getToken()->getUser();
     $em = $this->get('doctrine')->getManager();
-
-    // get related control/scenario/project entities for breadcrumb
     $control = $em->getRepository('FFN\MonBundle\Entity\Control')->findOneById($control_id);
+    
+    // get related control/scenario/project entities for breadcrumb
     if ($control instanceof ControlEntity) {
       $scenario = $control->getScenario();
       $project = $scenario->getProject();
       $title = $control->getName();
-    } else {
+    } 
+    else {
       // control identifier unknow
       $this->get('session')->getFlashBag()->add('error', $this->get('translator')->trans('mon_control_unknown'));
       return $this->redirect($this->generateUrl('mon_home'));
@@ -42,10 +48,18 @@ class GraphController extends Controller {
 
     // get graphic data from DB
     $graphdata = array();
+    
+    if (is_null($startTs)) {
+      $startTs = new DateTime("-12 hour", new \DateTimeZone('UTC'));
+    }
+    if (is_null($stopTs)) {
+      $stopTs = new DateTime("+12 hour", new \DateTimeZone('UTC'));
+    }
+    
     $captures_data = $em->getRepository('FFN\MonBundle\Entity\Capture')->findByIdAndTimeRange($control_id, $startTs, $stopTs);
+    
     if (count($captures_data) > 0) {
       foreach ($captures_data as $capture) {
-        $user = $this->get('security.context')->getToken()->getUser();
         if ($user === $capture->getControl()->getScenario()->getProject()->getUser()) {
           array_push($graphdata, $capture);
           $captureDetail  = $capture->getCaptureDetail();
@@ -60,9 +74,12 @@ class GraphController extends Controller {
     } else {
       $this->get('session')->getFlashBag()->add('error', $this->get('translator')->trans('mon_graph_empty'));
       return $this->render('FFNMonBundle:Page:Control\graph_empty.html.twig', array(
-                  'project' => $project,
-                  'scenario' => $scenario,
-                  'control' => $control,
+                  'project'   => $project,
+                  'scenario'  => $scenario,
+                  'control'   => $control,
+                  'start'     => $startTs,
+                  'stop'      => $stopTs,
+                  'timezone'  => $user->getTimezone()
           ));
     }
 
